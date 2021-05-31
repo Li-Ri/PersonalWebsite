23f015686fed4f24e37c1992687fc79dd215c8c1
Liam <liam.richens.11@gmail.com>
2021-05-31 21:38:18 +0100

    css styling of home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./components/About";
 import Blog from "./components/Blog";
 import Contact from "./components/Contact";
+import backgroundPhoto from "./images/personal_site_background.jpg";
 import "./App.css";
 
 function App() {
+  const style = { background: "url()" };
   return (
-    <div className="App grid-container">
+    <div
+      className="App grid-container"
+      style={{
+        background: `linear-gradient(130deg, rgba(255, 255, 255) 40%, transparent 10%), linear-gradient(to bottom, rgba(0, 0, 0,0.8) 10%, transparent), url(${backgroundPhoto})`,
+        backgroundSize: "cover",
+      }}
+    >
       <NavBar />
       <Router>
         <Switch>
@@ -18,7 +26,6 @@ function App() {
           <Route path="/contact" component={Contact} />
         </Switch>
       </Router>
-      <div className="previous-viewed">hello im the other grid</div>
     </div>
   );
 }
